c0eabe22a53142c89dadb49c97f3e6135cbac264
Lucas Eduardo <Lucaspity721@gmail.com>
2021-11-22 17:07:35 -0300

    Estilização: Alteração De Fontes E Alinhamentos Centrais e De Coluna. Acrescimo De Div's Para Serem Usadas Como Botões De Acesso Aos Jogos E Criados Com Base Em Um Vetor Denominado Jogos

--- src/Scripts/Script.js
+++ src/Scripts/Script.js
@@ -1,3 +1,5 @@
+var Jogos = ["Forca"];
+
 var body = document.querySelector('body');
 
 var ContainerSeletor = document.createElement('div');
@@ -9,3 +11,13 @@ TituloSeletor.setAttribute('class','TituloSeletor');
 TituloSeletor.textContent = 'Seleção De Jogos';
 ContainerSeletor.appendChild(TituloSeletor);
 
+for (let i = 0; i < Jogos.length; i++) {
+  var BntSeletores = document.createElement('div')
+  BntSeletores.setAttribute('class','BntSeletores');
+  ContainerSeletor.appendChild(BntSeletores);
+
+  var TitleJogos = document.createElement('p');
+  TitleJogos.setAttribute('class','TitleJogos');
+  BntSeletores.appendChild(TitleJogos);
+  TitleJogos.textContent = Jogos[i];
+}
