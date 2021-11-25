4270d19879b079d5d1e64f8db4c2cdf2c037b264
Lucas Eduardo <85133205+Lucas-LE1@users.noreply.github.com>
2021-11-25 19:28:24 -0300

    Update TesteDispositivo.js

--- src/Scripts/TesteDispositivo.js
+++ src/Scripts/TesteDispositivo.js
@@ -3,7 +3,7 @@ import { Container } from "./Script.js";
 export function DetectPhone() {
   var uagent = navigator.userAgent.toLowerCase();
   if (uagent.search("android" || "iphone") > -1) {
-    Container.style.width = window.outerWidth * 0.95;
+    Container.style.width = window.outerWidth * 0.5;
   } else {
     Container.style.width = "60%";
   }
