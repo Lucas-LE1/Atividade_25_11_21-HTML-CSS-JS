12e826296174befe382d740a2aa2064a307dce5e
Lucas Eduardo <Lucaspity721@gmail.com>
2021-11-24 22:16:53 -0300

    Acréscimo De Estilização, Div's De Caracteres, Preenchimento e Teste De Finalização

--- src/Scripts/Forca.js
+++ src/Scripts/Forca.js
@@ -1,4 +1,5 @@
 import { Container, Titulos } from "./Script.js";
+import { WordsForca } from "./WordsForcaScript.js";
 
 var Alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
@@ -7,6 +8,7 @@ var Detalhes = [
   "Cada letra que ele acerta é escrita no espaço correspondente.",
   "Caso a letra não exista nessa palavra, desenha-se uma parte do corpo (iniciando pela cabeça, tronco, braços…).",
 ];
+var index1 = Math.floor(Math.random() * WordsForca.length);
 var divContexto = document.createElement("ul");
 divContexto.setAttribute("class", "divContexto");
 
@@ -21,15 +23,60 @@ divJogoForca.setAttribute("class", "divJogoForca");
 
 var divForca = document.createElement("div");
 divForca.setAttribute("class", "divForca");
+divForca.textContent = WordsForca[index1].dica;
 
 var divLetras = document.createElement("div");
 divLetras.setAttribute("class", "divLetras");
 
-for (let i = 0; i < 4; i++) {
-  var Forca = document.createElement("div");
-  Forca.setAttribute("class", "Forca");
-  divForca.appendChild(Forca);
-  Forca.style = "background:green;widht:20px;height:20px;border:2px solid";
+var divCorpo = document.createElement("div");
+divCorpo.setAttribute("class", "divCorpo");
+divForca.appendChild(divCorpo);
+
+var divTraçados = document.createElement("div");
+divTraçados.setAttribute("class", "divTraçados");
+divForca.appendChild(divTraçados);
+
+function criarTraçados() {
+  let palavra = SortearLetra();
+  console.log(palavra);
+  for (let i = 0; i < palavra.length; i++) {
+    var divCaracter = document.createElement("div");
+    divCaracter.setAttribute("class", "DivCaracter");
+    var Caracter = document.createElement("p");
+    divTraçados.appendChild(divCaracter);
+    divCaracter.appendChild(Caracter);
+  }
+  var Caracters = document.querySelectorAll("div.DivCaracter");
+  let TestePalavra = Caracters.length;
+  for (let i = 0; i < Alfabeto.length; i++) {
+    var Letra = document.createElement("div");
+    Letra.setAttribute("class", "Letra");
+    divLetras.appendChild(Letra);
+    Letra.textContent = Alfabeto[i];
+  }
+  const Letras = document.querySelectorAll("div.Letra");
+
+  for (let index = 0; index < Letras.length; index++) {
+    Letras[index].addEventListener("click", () => {
+      divLetras.removeChild(Letras[index]);
+
+      if (palavra.search(Letras[index].innerHTML) == -1) {
+      } else {
+        for (let i = 0; i < palavra.length; i++) {
+          if (palavra[i] == Letras[index].innerHTML) {
+            Caracters[i].textContent = Letras[index].innerHTML;
+            TestePalavra--;
+          }
+        }
+      }
+      if (TestePalavra==0) {
+      }
+    });
+  }
+}
+function SortearLetra() {
+  let index2 = Math.floor(Math.random() * WordsForca[index1].Words.length);
+  return WordsForca[index1].Words[index2].toUpperCase();
 }
 
 export function JogoForca0() {
@@ -38,20 +85,7 @@ export function JogoForca0() {
   Container.appendChild(divJogoForca);
   divJogoForca.appendChild(divForca);
   divJogoForca.appendChild(divLetras);
-  Container.style.width = "70%";
+  Container.style.width = "60%";
   Titulos.textContent = "Jogo Da Forca";
-
-  for (let i = 0; i < Alfabeto.length; i++) {
-    var Letra = document.createElement("div");
-    Letra.setAttribute("class", "Letra");
-    divLetras.appendChild(Letra);
-    Letra.textContent = Alfabeto[i];
-  }
-  const Letras = document.querySelectorAll("div.Letra");
-  console.log(Letras.length);
-  for (let i = 0; i < Letras.length; i++) {
-    Letras[i].addEventListener("click", () => {
-      divLetras.removeChild(Letras[i]);
-    });
-  }
+  criarTraçados();
 }
