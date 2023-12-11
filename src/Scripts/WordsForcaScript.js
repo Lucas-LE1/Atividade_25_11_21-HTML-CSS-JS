267735c42b903e2916f969c0e5b6ba597c28b008
Lucas Eduardo <85133205+Lucas-LE1@users.noreply.github.com>
2023-12-11 19:45:22 -0300

    Update WordsForcaScript.js

--- src/Scripts/WordsForcaScript.js
+++ src/Scripts/WordsForcaScript.js
@@ -1,42 +1,83 @@
-export const WordsForca = [{
-    dica: "Animais",
-    Words:[
-        "Albatroz","Alpaca","Anchova", "Bacalhau",
-        "Badejo", "Barracuda","Beluga","Cagado","Chinchila","Craca",
-        "Dromedario","Escaravelho","Gerbo","Gnu",
-        "Gralha","Hamster","Lemure","Lhama",
-        "Lince","Marreco","Melro","Ocapi",
-        "Ourico","Pelicano","Percevejo","Pirilampo",
-        "Quati","Rouxinol","Sanguessuga","Surucucu",
-        "Tapir","Texugo","Vison","Zebu"]
-},
-{
-    dica:"Profissão",
-    Words:["Apicultor","Agrônomo",
-        "Auditor","Bartender","Cerimonialista",
-        "Chef","Coach","Desembargador",
-        "Despachante","Endocrinologista","Embaixador",
-        "Gerentólogo","Headhunter",
-        "Intérprete","Juiz","Nanotecnólogo",
-        "Nutrólogo","Pizzaiolo",
-        "Perito","Quiroprata",
-        "Roteirizador","Silvicultor","Trader",
-        "Taquígrafo","Turismólogo",]
-},
-{
-    dica:"Objetos",
-    Words:[
-"Ampulheta","Anzol",
-"Botija","Candelabro","Dedaleira",
-"Desfibrilador","Diapasão",'Echarpe',
-"Estribo",'Fagote',"Fantoche",
-"Fórceps","Freezer","Guidão",
-"Guidom","Navalha","Jaleco",
-"Modem","Narguilé","Nebulizador",
-"Novelo","Oxímetro",
-"Pêndulo","Quepe","Selim",
-"Sintetizador","Spray",
-"Vuvuzela","Webcam","Xadrez",
-"Xilofone","Zíper"
-]
-}]
+export const WordsForca = [
+    {
+        dica: "Animais",
+        Words: [
+            "Albatroz","Alpaca","Anchova", "Bacalhau",
+            "Badejo", "Barracuda","Beluga","Cagado","Chinchila","Craca",
+            "Dromedario","Escaravelho","Gerbo","Gnu",
+            "Gralha","Hamster","Lemure","Lhama",
+            "Lince","Marreco","Melro","Ocapi",
+            "Ourico","Pelicano","Percevejo","Pirilampo",
+            "Quati","Rouxinol","Sanguessuga","Surucucu",
+            "Tapir","Texugo","Vison","Zebu"
+        ]
+    },
+    {
+        dica:"Profissão",
+        Words:[
+            "Apicultor","Agrônomo",
+            "Auditor","Bartender","Cerimonialista",
+            "Chef","Coach","Desembargador",
+            "Despachante","Endocrinologista","Embaixador",
+            "Gerentólogo","Headhunter",
+            "Intérprete","Juiz","Nanotecnólogo",
+            "Nutrólogo","Pizzaiolo",
+            "Perito","Quiroprata",
+            "Roteirizador","Silvicultor","Trader",
+            "Taquígrafo","Turismólogo"
+        ]
+    },
+    {
+        dica:"Objetos",
+        Words:[
+            "Ampulheta","Anzol",
+            "Botija","Candelabro","Dedaleira",
+            "Desfibrilador","Diapasão",'Echarpe',
+            "Estribo",'Fagote',"Fantoche",
+            "Fórceps","Freezer","Guidão",
+            "Guidom","Navalha","Jaleco",
+            "Modem","Narguilé","Nebulizador",
+            "Novelo","Oxímetro",
+            "Pêndulo","Quepe","Selim",
+            "Sintetizador","Spray",
+            "Vuvuzela","Webcam","Xadrez",
+            "Xilofone","Zíper"
+        ]
+    },
+    {
+        dica:"Países",
+        Words:[
+            "Brasil", "Estados Unidos", "Canadá", "Japão",
+            "Austrália", "França", "Alemanha", "Índia",
+            "China", "Rússia", "Reino Unido", "Itália",
+            "Espanha", "México", "Argentina", "África do Sul",
+            "Coreia do Sul", "Arábia Saudita", "Nigéria",
+            "Indonésia", "Tailândia", "Turquia", "Suíça",
+            "Suécia", "Dinamarca", "Noruega", "Finlândia",
+            "Grécia", "Portugal", "Egito", "Israel",
+            "Nova Zelândia", "Áustria", "Irlanda", "Polônia",
+            "Bélgica", "Holanda", "Singapura", "Malásia",
+            "Vietnã", "Colômbia", "Chile", "Peru"
+        ]
+    },
+    {
+        dica:"Frutas",
+        Words:[
+            "Abacaxi", "Banana", "Cereja", "Damasco",
+            "Elderberry", "Figo", "Goiaba", "Hidromel",
+            "Icaco", "Jabuticaba", "Kiwi", "Laranja",
+            "Manga", "Nectarina", "Pêssego", "Quiuí",
+            "Romã", "Sapoti", "Tamarindo", "Uva"
+        ]
+    },
+    {
+        dica:"Marcas de Carro",
+        Words:[
+            "Audi", "BMW", "Chevrolet", "Dodge",
+            "Ferrari", "Honda", "Jaguar", "Kia",
+            "Lamborghini", "Maserati", "Nissan", "Opel",
+            "Porsche", "Renault", "Subaru", "Tesla",
+            "Toyota", "Volvo", "Volkswagen", "Fiat"
+        ]
+    }
+];
